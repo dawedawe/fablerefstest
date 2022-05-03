72290b457a6f1a6af90addc8d4194ee6638210a5
David Schaefer <david.schaefer@rhein-spree.com>
2022-05-03 08:04:50 +0200

    initial import

---
+++ lib/Class.cs
@@ -0,0 +1,14 @@
+﻿namespace Lib
+{
+    public class Class
+    {
+        public static int Add(int x, int y) => x + y;
+    }
+
+    public enum Stuff
+    {
+        CoolStuff = 0,
+        BadStuff = 1,
+        AwesomeStuff = 2
+    }
+}
